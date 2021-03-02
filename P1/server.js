069fcb0679f105c3a8d69e132dd3fa7c53657e25
yolandalillo <yolandalillom@gmail.com>
2021-03-02 14:45:37 +0100

    Empezando servidor básico

--- P1/server.js
+++ P1/server.js
@@ -1,2 +1,14 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
 const PUERTO = 8080
+
+const server = http.createServer((req,ser) => {
+    console.log("Petición recibida");
+
+})
+
+server.listen(PUERTO);
+
+console.log("Servidor escuchando en " + PUERTO)
