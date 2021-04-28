6ebb74d4b01f834815794b4db3830f4175281245
yolandalillo <yolandalillom@gmail.com>
2021-04-28 16:56:23 +0200

    Avanzando en el servidor

--- P2/tienda.js
+++ P2/tienda.js
@@ -3,18 +3,40 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-//-- Puerto que vamos a utilizar
+//-- Puerto
 const PUERTO = 8080;
 
-//-- Mensaje 
-console.log("Arrancando servidor...");
+//-- Página principal de la web
+const PRINCIPAL = fs.readFileSync('principal.html', 'utf-8');
 
-//-- Nombre del fichero JSON a leer
-const FICHERO_JSON = "tienda.json"
+//-- Registro -> Fichero JSON
+const FICHERO_JSON = "tienda.json";
 
-//-- Creamos el servidor 
-const server = http.createServer((req, res) => {
-    //-- Construimos url con la url de solicitud
-    const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+//-- Página web del formulario login
+const FORMULARIO = fs.readFileSync('form.html','utf-8');
+
+//-- Páginas de respuesta (HACER)
+//-- Login ON
+//-- Login OFF
 
 
+//-- Estructura tienda a partir del contenido del fichero
+const tienda = JSON.parse(tienda_json);
+
+//-- Usuarios registrados.
+let usuarios_registrados = [];
+console.log("Lista de los usuarios registrados");
+
+tienda[1]["usuarios"].forEach((element, index)=>{
+    console.log("Usuario " + (index + 1) + ": " + element.usuario);
+    usuarios_registrados.push(element.usuario);
+});
+
+console.log();
+
+//-- Creamos el servidor
+
+
+
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
