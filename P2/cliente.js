b349fc4b5b5494291abf0a6283131a4089f162d1
yolandalillo <yolandalillom@gmail.com>
2021-05-10 18:14:15 +0200

    Falta JS y JSON

--- P2/cliente.js
+++ P2/cliente.js
@@ -4,3 +4,22 @@ const display1 = document.getElementById("display1");
 
 //-- Caja de busqueda
 const caja = document.getElementById("caja");
+
+//-- Función de retrollamada
+caja.oninput = () => {
+
+
+
+
+    //-- La peticion se realia solo si hay al menos 1 carácter
+    if (caja.value.length >= 1) {
+        //-- Configurar la petición
+        m.open("GET","/productos?param1=" + caja.value, true);
+        //-- Enviar la petición!
+        m.send();
+    } else {
+          display1.innerHTML="";
+    }
+
+}
+
--- P2/tienda.js
+++ P2/tienda.js
@@ -9,11 +9,30 @@ const PUERTO = 8080;
 //-- Página principal de la web
 const PRINCIPAL = fs.readFileSync('principal.html', 'utf-8');
 
+//-- Definir los tipos de mime
+const mime_type = {
+    "html" : "text/html",
+    "css"  : "text/css",
+    "js"   : "application/javascript",
+    "jpg"  : "image/jpg",
+    "JPG"  : "image/jpg",
+    "jpeg" : "image/jpeg",
+    "PNG"  : "image/png",
+    "png"  : "image/png",
+    "ico"  : "image/x-icon",
+    "json" : "application/json",
+};
+
+// Cargar página de error
+const ERROR_PAGE = fs.readFileSync('error.html','utf-8');
 //-- Registro -> Fichero JSON
 const FICHERO_JSON = "tienda.json";
 
-//-- Página web del formulario login
-const FORMULARIO = fs.readFileSync('form.html','utf-8');
+//-- Cargar las paginas de los productos
+const PRODUCTO1 = fs.readFileSync('producto1.html', 'utf-8');
+const PRODUCTO2 = fs.readFileSync('producto2.html', 'utf-8');
+const PRODUCTO3 = fs.readFileSync('producto3.html', 'utf-8');
+const PRODUCTO4 = fs.readFileSync('producto4.html', 'utf-8');
 
 //-- Páginas de respuesta (HACER)
 //-- Login ON
