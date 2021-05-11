027caa2b7b47449285b726ebfeaa6d46eadb7982
yolandalillo <yolandalillom@gmail.com>
2021-05-11 13:20:31 +0200

    Utilizando S7 para acabar cliente

--- P2/cliente.js
+++ P2/cliente.js
@@ -7,10 +7,39 @@ const caja = document.getElementById("caja");
 
 //-- Función de retrollamada
 caja.oninput = () => {
+    //-- Crear objeto para hacer peticiones AJAX
+    const m = new XMLHttpRequest();
 
-
-
-
+    //-- Función de callback que se invoca cuando
+    //-- hay cambios de estado en la petición
+    m.onreadystatechange = () => {
+        //-- Petición enviada y recibida. Todo OK!
+        if (m.readyState==4) {
+            //-- Solo la procesamos si la respuesta es correcta
+            if (m.status==200) {
+                //-- La respuesta es un objeto JSON
+                let productos = JSON.parse(m.responseText)
+                console.log(productos);
+                //-- Borrar el resultado anterior
+                display1.innerHTML = "";
+                //--Recorrer los productos del objeto JSON
+                for (let i=0; i < productos.length; i++) {
+                    //-- Añadir cada producto al display de visualización
+                    display1.innerHTML += productos[i];
+                    //-- Separamos los productos por ',''
+                    if (i < productos.length-1) {
+                        display1.innerHTML += ',';
+                    }
+                }
+            }else{
+                //-- Petición errónea
+                //-- Notificamos en la consola y en la propia web
+                console.log("Error en la petición: " + m.status + " " + m.statusText);
+                display2.innerHTML += '<p>ERROR</p>'
+            }
+        }
+    }
+    console.log(caja.value.length);
     //-- La peticion se realia solo si hay al menos 1 carácter
     if (caja.value.length >= 1) {
         //-- Configurar la petición
@@ -18,8 +47,6 @@ caja.oninput = () => {
         //-- Enviar la petición!
         m.send();
     } else {
-          display1.innerHTML="";
+        display1.innerHTML="";
     }
-
 }
-
